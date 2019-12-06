55895d19cb2d2cb26dc3438b85ffb99305a36720
bushair <bushairkm007@@gmail.com>
2019-12-06 13:08:29 -0500

    Atribute Edt_UserName

--- app/src/main/java/com/example/group4_f2019_mad3125_fp/MainActivity.java
+++ app/src/main/java/com/example/group4_f2019_mad3125_fp/MainActivity.java
@@ -1,16 +1,28 @@
 package com.example.group4_f2019_mad3125_fp;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.graphics.Color;
+import android.content.Intent;
+import android.content.DialogInterface;
+import android.content.*;
+import android.widget.CompoundButton;
+public class MainActivity extends  AppCompatActivity  {
+    private EditText edtUsername;
+    private EditText edtPassword;
+    private Button btnLogin;
+    public static  final String KEY_NAME="name";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //srinath
-        //srinath
-    }
+        edtUsername=findViewById(R.id.edt_UserName);
+       
 }
